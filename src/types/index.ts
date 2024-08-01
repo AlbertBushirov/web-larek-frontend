c4449c48474bb438069861532136c6f79dc84ac0
Albert Bashirov <albash7@yandex.ru>
2024-08-01 12:51:49 +0500

    Readme Update

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,29 +1,41 @@
-// Товар
-export interface Iproduct {
-id: string;
-title: string;
-price: number;
-image: string;
-description: string;
-category: string;
-payment: string;
-address: string;
-}
-
-// Покупатель
-export interface Ibuyer {
-email: string;
-phone: number;
-}
-
-// Корзина 
-export interface Ibasket {
-items: HTMLElement[];
-total: number;
-}
-
-// Заказ 
-export interface IOrder {
-payment: string;
-address: string;
-}
+// Приложение
+export interface ILarekApp {
+	basket: IBasket[];
+	cardsList: ICardItem[];
+	preview: string | null;
+	order: IOrder | null;
+  }
+  
+  // Товар
+  export interface ICardItem {
+	id: string;
+	description: string;
+	image: string;
+	title: string;
+	category: string;
+	price: number | null;
+	inBasket: boolean;
+  }
+  
+  // Данные заказа
+  export interface IOrder {
+	payment: string;
+	address: string;
+	email: string;
+	phone: string;
+	total: number;
+	id: string[];
+  }
+  
+  // Корзина
+  export interface IBasket {
+	quantity: number;
+	title: string;
+	price: number;
+	totalPrice: number;
+  }
+  
+  export type FormErrors = Partial<Record<keyof IOrder, string>>;
+  
+  // Список используемых констант
+  export enum EventTypes {}
